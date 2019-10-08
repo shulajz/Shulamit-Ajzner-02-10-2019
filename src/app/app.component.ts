faa66416ead889a09fe54974fe318700d3a00cbb
shulajz <shulajz@gmail.com>
2019-10-08 12:05:19 +0300

    added features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FavoritesService} from './services/favorites.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Shulamit-Ajzner';
+  constructor(private favoritesService: FavoritesService) {
+    this.favoritesService.initBooleanOfFavoriteExists();
+  }
+
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainComponent} from './components/main/main.component';
+import {FavoritesComponent} from './components/favorites/favorites.component';
+import {NavigationComponent} from './components/navigation/navigation.component';
+import {RouterModule} from '@angular/router';
+import {WeatherService} from './services/weather.service';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule, MatInputModule, MatAutocompleteModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import {MatAutocomplete} from '@angular/material';
+
+const appRoutes = [
+  {
+    path: '', component: MainComponent
+  },
+  {
+    path: 'favorites', component: FavoritesComponent
+  }
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FavoritesComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
